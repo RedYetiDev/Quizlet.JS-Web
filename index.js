8c37b609a00f554f76f597e05bcd1ea5ad631cdb
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2021-11-13 09:02:51 -0500

    Production JS

--- index.js
+++ index.js
@@ -1,164 +1,194 @@
-const animalNames = {
-    alligator: "Alligators",
-    alpaca: "Alpacas",
-    bear: "Bears",
-    bison: "Bison",
-    camel: "Camels",
-    chameleon: "Chameleons",
-    cheetah: "Cheetahs",
-    dolphin: "Dolphins",
-    eagle: "Bald Eagles",
-    fox: "Foxes",
-    frog: "Frogs",
-    giraffe: "Giraffes",
-    hedgehog: "Hedgehogs",
-    kangaroo: "Kangaroos",
-    koala: "Koalas",
-    lion: "Lions",
-    lynx: "Lynx",
-    mantaray: "Stingrays",
-    mountainlion: "Pumas",
-    mustang: "Mustangs",
-    orca: "Orcas",
-    ostrich: "Ostriches",
-    ox: "Oxen",
-    panda: "Pandas",
-    peacock: "Peacocks",
-    penguin: "Penguins",
-    puffin: "Puffins",
-    reindeer: "Reindeer",
-    rhino: "Rhinos",
-    seadragon: "Sea Dragons",
-    shark: "Sharks",
-    siberiantiger: "Siberian Tigers",
-    trex: "T-Rexes",
-    tiger: "Tigers",
-    turtle: "Sea Turtles",
-    unicorn: "Unicorns",
-    wolf: "Wolves",
-    zebra: "Zebras"
-}
+"use strict";
 
+const animalNames = {
+  alligator: "Alligators",
+  alpaca: "Alpacas",
+  bear: "Bears",
+  bison: "Bison",
+  camel: "Camels",
+  chameleon: "Chameleons",
+  cheetah: "Cheetahs",
+  dolphin: "Dolphins",
+  eagle: "Bald Eagles",
+  fox: "Foxes",
+  frog: "Frogs",
+  giraffe: "Giraffes",
+  hedgehog: "Hedgehogs",
+  kangaroo: "Kangaroos",
+  koala: "Koalas",
+  lion: "Lions",
+  lynx: "Lynx",
+  mantaray: "Stingrays",
+  mountainlion: "Pumas",
+  mustang: "Mustangs",
+  orca: "Orcas",
+  ostrich: "Ostriches",
+  ox: "Oxen",
+  panda: "Pandas",
+  peacock: "Peacocks",
+  penguin: "Penguins",
+  puffin: "Puffins",
+  reindeer: "Reindeer",
+  rhino: "Rhinos",
+  seadragon: "Sea Dragons",
+  shark: "Sharks",
+  siberiantiger: "Siberian Tigers",
+  trex: "T-Rexes",
+  tiger: "Tigers",
+  turtle: "Sea Turtles",
+  unicorn: "Unicorns",
+  wolf: "Wolves",
+  zebra: "Zebras"
+};
 var Game;
 var autoCorrect = false;
 var showCorrect = false;
 
 function runGame() {
-    ReactDOM.render(loadingScreen, document.getElementById('root'))
-    var pin = document.getElementById('pin').value
-    var name = document.getElementById('name').value
-    autoCorrect = document.getElementById('autoCorrect').checked
-    showCorrect = document.getElementById('showCorrect').checked
-    try {
-        Game = new Quizlet(pin, name);
-    } catch(e) {
-        alert(e);
-        location.reload()
+  ReactDOM.render(loadingScreen, document.getElementById('root'));
+  var pin = document.getElementById('pin').value;
+  var name = document.getElementById('name').value;
+  autoCorrect = document.getElementById('autoCorrect').checked;
+  showCorrect = document.getElementById('showCorrect').checked;
+
+  try {
+    Game = new Quizlet(pin, name);
+  } catch (e) {
+    alert(e);
+    location.reload();
+  }
+
+  delete Game.callbacks.error[0];
+  Game.on('error', error => {
+    alert(error);
+    location.reload();
+  });
+  Game.joinGame();
+  Game.on('connect', () => {
+    ReactDOM.render(waitingScreen, document.getElementById('root'));
+  });
+  Game.on('teamAssignments', (teamName, playerNames) => {
+    var teamScreen = /*#__PURE__*/React.createElement("div", {
+      className: "container text-center"
+    }, /*#__PURE__*/React.createElement("h1", null, "You're In!"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h4", {
+      className: "text-secondary"
+    }, "The Host has assigned teams!"), /*#__PURE__*/React.createElement("h4", null, "You are on the ", animalNames[teamName], " with ", playerNames.join(', ')));
+    ReactDOM.render(teamScreen, document.getElementById('root'));
+  });
+  Game.on('question', (questionName, possibleAnswers, correctAnswer) => {
+    renderQuestionInfo(questionName, possibleAnswers, correctAnswer);
+
+    if (autoCorrect) {
+      Game.answer(correctAnswer);
     }
-    delete Game.callbacks.error[0]
-    Game.on('error', (error) => {
-        alert(error);
-        location.reload();
-    })
-    Game.joinGame()
-    Game.on('connect', () => {
-        ReactDOM.render(waitingScreen, document.getElementById('root'))
-    })
-    Game.on('teamAssignments', (teamName, playerNames) => {
-        var teamScreen = (
-            <div className="container text-center">
-                <h1>You're In!</h1>
-                <br />
-                <h4 className="text-secondary">The Host has assigned teams!</h4>
-                <h4>You are on the {animalNames[teamName]} with {playerNames.join(', ')}</h4>
-            </div>
-        )
-        ReactDOM.render(teamScreen, document.getElementById('root'))
-    })
-    Game.on('question', (questionName, possibleAnswers, correctAnswer) => {
-        renderQuestionInfo(questionName, possibleAnswers, correctAnswer)
-        if (autoCorrect) {
-            Game.answer(correctAnswer)
-        }
-    })
-    Game.on('gameOver', (didWin) => {
-        var gameOverScreen = <h1 className="text-center">Game Over! {didWin ? "You Won!" : "You Lost!" }</h1>
-        ReactDOM.render(gameOverScreen, document.getElementById("root"))
-    })
-    Game.on('answer', (isCorrect, answerer) => {
-        var nextQuestionScreen = (
-            <div className="container text-center">
-                <h1>Waiting Next Question</h1>
-                <h2>{answerer} answered <span className={isCorrect ? "text-success" : "text-danger"}>{isCorrect ? "correctly" : "incorrectly"}</span></h2>
-            </div>
-        )
-        ReactDOM.render(nextQuestionScreen, document.getElementById('root'))
-    })
+  });
+  Game.on('gameOver', didWin => {
+    var gameOverScreen = /*#__PURE__*/React.createElement("h1", {
+      className: "text-center"
+    }, "Game Over! ", didWin ? "You Won!" : "You Lost!");
+    ReactDOM.render(gameOverScreen, document.getElementById("root"));
+  });
+  Game.on('answer', (isCorrect, answerer) => {
+    var nextQuestionScreen = /*#__PURE__*/React.createElement("div", {
+      className: "container text-center"
+    }, /*#__PURE__*/React.createElement("h1", null, "Waiting Next Question"), /*#__PURE__*/React.createElement("h2", null, answerer, " answered ", /*#__PURE__*/React.createElement("span", {
+      className: isCorrect ? "text-success" : "text-danger"
+    }, isCorrect ? "correctly" : "incorrectly")));
+    ReactDOM.render(nextQuestionScreen, document.getElementById('root'));
+  });
 }
 
-function renderQuestionInfo(name,choices,answer) {
-    var questionScreenPart =  <h2>{name}</h2>
-    var choiceCards = []
-    choices.forEach((choice, idx) => {
-        choiceCards.push(choiceTemplate(choice, idx, choice == answer, false))
-    })
-    var choiceScreenPart = (
-        <div className="d-grid gap-2">
-            {choiceCards}
-        </div>
-    )
-    var questionScreen = (
-        <div className="container text-center">
-            {questionScreenPart}
-            {choiceScreenPart}
-        </div>
-    )
-    ReactDOM.render(questionScreen, document.getElementById("root"))
+function renderQuestionInfo(name, choices, answer) {
+  var questionScreenPart = /*#__PURE__*/React.createElement("h2", null, name);
+  var choiceCards = [];
+  choices.forEach((choice, idx) => {
+    choiceCards.push(choiceTemplate(choice, idx, choice == answer, false));
+  });
+  var choiceScreenPart = /*#__PURE__*/React.createElement("div", {
+    className: "d-grid gap-2"
+  }, choiceCards);
+  var questionScreen = /*#__PURE__*/React.createElement("div", {
+    className: "container text-center"
+  }, questionScreenPart, choiceScreenPart);
+  ReactDOM.render(questionScreen, document.getElementById("root"));
 }
 
 function choiceTemplate(choice, key, isCorrect, isDisabled) {
-    if (!choice) return ""
-    if (isCorrect && showCorrect) {
-        return <button onClick={() => Game.answer(choice)} className="btn btn-block btn-success" key={key}>{choice}</button>
-    } else if (!isCorrect && showCorrect) {
-        return <button onClick={() => Game.answer(choice)} className="btn btn-block btn-danger" key={key}>{choice}</button>
-    } else {
-        return <button onClick={() => Game.answer(choice)} className="btn btn-block btn-primary" key={key}>{choice}</button>
-    }
+  if (!choice) return "";
+
+  if (isCorrect && showCorrect) {
+    return /*#__PURE__*/React.createElement("button", {
+      onClick: () => Game.answer(choice),
+      className: "btn btn-block btn-success",
+      key: key
+    }, choice);
+  } else if (!isCorrect && showCorrect) {
+    return /*#__PURE__*/React.createElement("button", {
+      onClick: () => Game.answer(choice),
+      className: "btn btn-block btn-danger",
+      key: key
+    }, choice);
+  } else {
+    return /*#__PURE__*/React.createElement("button", {
+      onClick: () => Game.answer(choice),
+      className: "btn btn-block btn-primary",
+      key: key
+    }, choice);
+  }
 }
 
-const loadingScreen = (
-    <div className="text-center">
-        <div className="spinner-border" style={{width: "25rem", height: "25rem"}} role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-    </div>
-)
-
-const pinScreen = (
-    <div className="container text-center">
-        <br />
-        <div className="mb-3">
-            <input id="pin" className="form-control" type="text" maxLength="6" placeholder="Game PIN"></input>
-        </div>
-        <div className="mb-3">
-            <input id="name" className="form-control" type="text" placeholder="Display Name"></input>
-        </div>
-        <div className="mb-3 btn-group form-control">
-            <input id="autoCorrect" className="btn-check" type="checkbox"></input>
-            <label className="btn btn-outline-success" htmlFor="autoCorrect">Auto Answer Correctly</label>
-            <input id="showCorrect" className="btn-check" type="checkbox"></input>
-            <label className="btn btn-outline-success" htmlFor="showCorrect">Show Correct Answers</label>
-        </div>
-        <button className="btn btn-primary form-control" onClick={runGame}>Join Game</button>
-    </div>
-)
-const waitingScreen = (
-    <div className="container text-center">
-        <h1>You're In!</h1>
-        <br />
-        <h4 className="text-secondary">Waiting for the host to start the game</h4>
-    </div>
-)
-
-ReactDOM.render(pinScreen, document.getElementById('root'))
+const loadingScreen = /*#__PURE__*/React.createElement("div", {
+  className: "text-center"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "spinner-border",
+  style: {
+    width: "25rem",
+    height: "25rem"
+  },
+  role: "status"
+}, /*#__PURE__*/React.createElement("span", {
+  className: "visually-hidden"
+}, "Loading...")));
+const pinScreen = /*#__PURE__*/React.createElement("div", {
+  className: "container text-center"
+}, /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("div", {
+  className: "mb-3"
+}, /*#__PURE__*/React.createElement("input", {
+  id: "pin",
+  className: "form-control",
+  type: "text",
+  maxLength: "6",
+  placeholder: "Game PIN"
+})), /*#__PURE__*/React.createElement("div", {
+  className: "mb-3"
+}, /*#__PURE__*/React.createElement("input", {
+  id: "name",
+  className: "form-control",
+  type: "text",
+  placeholder: "Display Name"
+})), /*#__PURE__*/React.createElement("div", {
+  className: "mb-3 btn-group form-control"
+}, /*#__PURE__*/React.createElement("input", {
+  id: "autoCorrect",
+  className: "btn-check",
+  type: "checkbox"
+}), /*#__PURE__*/React.createElement("label", {
+  className: "btn btn-outline-success",
+  htmlFor: "autoCorrect"
+}, "Auto Answer Correctly"), /*#__PURE__*/React.createElement("input", {
+  id: "showCorrect",
+  className: "btn-check",
+  type: "checkbox"
+}), /*#__PURE__*/React.createElement("label", {
+  className: "btn btn-outline-success",
+  htmlFor: "showCorrect"
+}, "Show Correct Answers")), /*#__PURE__*/React.createElement("button", {
+  className: "btn btn-primary form-control",
+  onClick: runGame
+}, "Join Game"));
+const waitingScreen = /*#__PURE__*/React.createElement("div", {
+  className: "container text-center"
+}, /*#__PURE__*/React.createElement("h1", null, "You're In!"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h4", {
+  className: "text-secondary"
+}, "Waiting for the host to start the game"));
+ReactDOM.render(pinScreen, document.getElementById('root'));
