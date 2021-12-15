a61fe8acdc172b407e8aca3bb2c0f2a181cbcce3
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2021-12-14 19:34:33 -0500

    Fix

--- quizlet.web.js
+++ quizlet.web.js
@@ -88,6 +88,52 @@ function _ws(url, options) {
     return new WebSocket(url)
 }
 
+class _Live extends _EventEmitter {
+    constructor(pin, name, opt = {}) {
+        if (!pin) throw new Error("No PIN Provided")
+        super()
+        this.pin = pin.toString().replace('-', '');
+        if (!name && !opt.accountName) console.warn('No name provided, defaulting to "Quizlet.JS Bot"')
+        this.name = name || "Quizlet.JS Bot";
+        this.round = 0;
+        this.streak = 0;
+        this.userImage = opt.userImage || "https://quizlet.com/favicon.ico"
+        this.accountName = opt.accountName || undefined
+    }
+
+    async getTokenAndId() {
+
+        var headers = {
+            'User-Agent': 'Quizlet.JS'
+        }
+
+        if (this.accountName) {
+            var data = await _got(`https://quizlet.com/webapi/3.2/users?filters={"username":"${this.accountName}"}`, { headers }).json();
+            this.accountInfo = data.responses[0].models.user[0]
+            if (!this.accountInfo) this.emit('error','Invalid Account Username');
+            headers.Cookie = `ab.storage.userId.6f8c2b67-8bd5-42f6-9c5f-571d9701f693={"g":"${this.accountInfo.id}"}`
+            this.name = this.accountInfo.firstName
+            this.userImage = this.accountInfo._imageUrl
+        }
+
+        var data = await _got("https://quizlet.com/live", {
+            headers: headers
+        }).text();
+
+        return {
+            token: data.split(`"multiplayerToken":"`)[1].split(`",`)[0],
+            playerId: data.split('uid')[1].split(`"`)[2],
+            cpToken: data.split(`"checkpointToken":"`)[1].split(`",`)[0],
+            uid: data.split(`"uid":"`)[1].split(`",`)[0]
+        }
+    }
+
+    async leave() {
+        await this.socket.close();
+        this.emit('disconnect');
+    }
+}
+
 function require(req) {
     if (req == "events") {
         return _EventEmitter
@@ -95,6 +141,8 @@ function require(req) {
         return _got
     } else if (req == "ws") {
         return _ws
+    } else if (req == "./live") {
+        return _Live
     }
 }
 
